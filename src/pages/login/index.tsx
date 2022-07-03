4cb3d2b41704797a6ccf66b939abfd28622cf56e
VaynePeng <vaynepeng@gmail.com>
2022-07-04 02:53:09 +0800

    test

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -59,7 +59,7 @@ const Login: FC = (): ReactElement => {
             disableElevation
             onClick={loginHandle}
           >
-            登录
+            登录 哈哈哈
           </Button>
           <div className="agreement-context">
             <Checkbox
